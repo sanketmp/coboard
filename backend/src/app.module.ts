2a8f8145279ff59407bc968e81e7e22543b699f0
sanketmp <sanketmpol@gmail.com>
2026-06-30 22:45:12 +0530

    remove static module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 import { WhiteboardModule } from './whiteboard/whiteboard.module';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'client'),
-      serveStaticOptions: { etag: false, maxAge: 0 },
-    }),
-    WhiteboardModule,
-  ],
+  imports: [WhiteboardModule],
 })
 export class AppModule {}
