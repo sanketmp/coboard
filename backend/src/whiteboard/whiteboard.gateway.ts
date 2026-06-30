3d3f9a5a46a1ffefd9c03eb8df86f0935009412c
sanketmp <sanketmpol@gmail.com>
2026-06-30 21:08:47 +0530

    add vercel to cors

--- backend/src/whiteboard/whiteboard.gateway.ts
+++ backend/src/whiteboard/whiteboard.gateway.ts
@@ -11,7 +11,7 @@ import { WhiteboardService } from './whiteboard.service';
 
 @WebSocketGateway({ cors: {
   origin:[
-    'http://localhost:3000'
+    'http://localhost:3000','https://coboard-eight.vercel.app'
   ],
 } })
 export class WhiteboardGateway implements OnGatewayDisconnect {
